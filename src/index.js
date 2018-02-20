d84874871bf381373a449f1b44c4af8a7ee94e1a
Serj Nickey <serjnickey@gmail.com>
2018-02-20 20:15:04 +0300

    love triangle 1st

--- src/index.js
+++ src/index.js
@@ -4,4 +4,13 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+    let sum = 0;
+
+    preferences.forEach(function(item, i, array) {
+        if(item > 0 && array[item-1] > 0 && i+1 !== array[item-1] && i+1 === array[array[item-1]-1]) {
+            sum++;
+        }
+    });
+
+    return sum/3;
 };
